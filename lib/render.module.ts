117f4067661b6a1c94e3e1a875409298bea4775b
Vasil Rangelov <vasil.tech@condor-gaming.com>
2020-03-10 09:56:20 +0200

    Added a deprecation warning, outputted unconditionally on the error stream if register() is called. Also added doc comments for the two methods, including a "deprecated" tag, to assist IDEs.

--- lib/render.module.ts
+++ lib/render.module.ts
@@ -9,6 +9,12 @@ import { RendererConfig } from './types';
   providers: [RenderService],
 })
 export class RenderModule {
+  /**
+   * Registers this module with a Next app at the root of the Nest app.
+   *
+   * @param next The Next app to register.
+   * @param options Options for the RenderModule.
+   */
   public static async forRootAsync(
     next: ReturnType<typeof Server>,
     options: Partial<RendererConfig> = {},
@@ -57,11 +63,26 @@ export class RenderModule {
     private readonly service: RenderService,
   ) {}
 
+  /**
+   * Register the RenderModule.
+   *
+   * @deprecated Use RenderModule.forRootAsync() when importing the module, and remove this post init call.
+   * @param _app Previously, the Nest app. Now ignored.
+   * @param next The Next app.
+   * @param options Options for the RenderModule.
+   */
   public register(
     _app: any,
     next: ReturnType<typeof Server>,
     options: Partial<RendererConfig> = {},
   ) {
+    console.error(
+      'RenderModule.register() is deprecated and will be removed in a future release.',
+    );
+    console.error(
+      'Please use RenderModule.forRootAsync() when importing the module, and remove this post init call.',
+    );
+
     if (!this.service.isInitialized()) {
       this.service.mergeConfig(options);
       this.service.setRequestHandler(next.getRequestHandler());
